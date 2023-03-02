f4d1f8b88eeeebb5b15845a3e7ee41ffdec29177
Markos-Th09 <87304130+Markos-Th09@users.noreply.github.com>
2023-03-02 08:57:51 +0400

    Update for version 3.1

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub fn compile_code(mut code: String) -> Result<String, String> {
 	let options = Options::default();
 	let filename = String::from("(clue wasm)");
 	let code = unsafe { code.as_bytes_mut() };
-	let (codes, variables, ..) = preprocess_code(code, 1, false, &filename)?;
+	let (codes, variables, ..) = preprocess_code(code, 1, false, &filename, &options)?;
 	let code = preprocess_codes(0, codes, &variables, &filename)?;
 	let tokens = scan_code(code, &filename)?;
 	let (ctokens, statics) = parse_tokens(tokens, &filename, &options)?;
